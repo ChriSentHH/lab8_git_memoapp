80e2541579492f283e31661647dc84fd25f07300
ChriSnt <nakamu_cet@hotmail.com>
2019-12-18 13:56:05 +0900

    starting memoapp

---
+++ memoapp/memoapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memoapp
+//
+//  Created by Chris on 2019/12/18.
+//  Copyright © 2019 chris.nakamu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
